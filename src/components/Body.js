6f4ead96b996d49dabec65e6d6b11984ac249cef
Tilak Kumar Vasa <vasatilakkumar24@gmail.com>
2026-06-02 00:09:01 +0530

    exploring world of react and introduced useEffect, fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,67 @@
 import RestaurentCard from "./RestaurentCard";
-import {res_list} from "../utils/mockdata";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-    const[restaurentList, setRestaurentList] = useState(res_list);
+    const[restaurentList, setRestaurentList] = useState([]);
+    const[searchText, setSearchText] = useState("");
+    const[filteredRest, setFilteredRest] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=14.6688999&lng=77.5922514&collection=83649&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+        );
+
+        const json = await data.json();
+
+        setRestaurentList(json?.data?.cards.filter(c => c?.card?.card?.info));
+        setFilteredRest(json?.data?.cards.filter(c => c?.card?.card?.info));
+    };
+
+    if(restaurentList.length === 0) {
+        return <Shimmer/>
+    }
+
     return(
         <div className="body">
             <div className="top-rate-res-filt-btn">
+                <div className="search-container">
+                <input
+                    className="search-inpt"
+                    value={searchText}
+                    onChange={(e)=>setSearchText(e.target.value)}
+                />
+                <button
+                    className="search-btn"
+                    onClick={() => {
+                        const searchedrest = restaurentList.filter( (res) =>  
+                            res.card.card.info.name.toLowerCase()
+                            .includes(searchText.toLowerCase())
+                        );
+                        setFilteredRest(searchedrest);
+                    }}
+                >
+                    Search
+                </button>
+                </div>
                 <button
                     onClick={() => {
-                        console.log("clicked");
-                        
                         const topRatedres = restaurentList.filter((res)=>{
-                            const {avgRating} = res.card.card.info;
-                            return avgRating > 4.1;
+                            return res.card.card.info.avgRating > 4.1;
                         } );
-                        setRestaurentList(topRatedres);
+                        setFilteredRest(topRatedres);
                     }}
                 >
                     Top rated restaurents
                 </button>
             </div>
             <div className="res-list">
-                {restaurentList.map((restaurent)=>{
-                    const {id} = restaurent.card.card.info;
-                    return <RestaurentCard key={id} resData={restaurent} />;
+                {filteredRest.map((restaurent)=>{
+                    return <RestaurentCard key={restaurent.card.card.info.id} resData={restaurent} />;
                 })}
             </div>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { APP_HEADER_LOGO } from "../utils/constants";
 
 const Header = () => {
+    const[lgnLgtBtn, setLgnLgtBtn] = useState("Login");
     return(
         <div className="header">
         <div className="logo-container">
@@ -13,6 +15,16 @@ const Header = () => {
                 <li>About</li>
                 <li>Contact us</li>
                 <li>Cart</li>
+                <button
+                    onClick={() => {
+                        setLgnLgtBtn(lgnLgtBtn === "Login"
+                            ?"Logout"
+                            :"Login"
+                        )
+                    }}
+                >
+                    {lgnLgtBtn}
+                </button>
             </ul>
         </div>
         </div>
