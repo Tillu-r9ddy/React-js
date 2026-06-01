0f8889c7803d856e0a663179e1e03f8c0d455243
Tilak Kumar Vasa <vasatilakkumar24@gmail.com>
2026-06-02 00:38:37 +0530

    solved corsproxy issue without using corsproxy plugin

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
 
     const fetchData = async () => {
         const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=14.6688999&lng=77.5922514&collection=83649&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+            "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4470457&lng=78.3778342&collection=83649&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
         );
 
         const json = await data.json();
